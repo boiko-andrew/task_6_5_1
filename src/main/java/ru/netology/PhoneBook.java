4e74a4e6581a5e8c6d072e01baaafc8e18acea5b
Andrew Boiko <boiko_andrew@mail.ru>
2023-10-08 16:45:39 +0300

    Stub and tests for add

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -1,4 +1,16 @@
 package ru.netology;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private Map<String, String> nameMap;
+
+    public PhoneBook() {
+        this.nameMap = new HashMap<>();
+    }
+
+    public int add(String name, String number) {
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,43 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import ru.netology.PhoneBook;
+
+import java.util.stream.Stream;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class PhoneBookTest {
+    PhoneBook phoneBook;
+
+    @BeforeAll()
+    public void beforeAll() {
+        phoneBook = new PhoneBook();
+    }
+
+    @ParameterizedTest
+    @MethodSource("checkAddMethodSource")
+    public void parametrizedCheckAdd(String name, String number, int expectedQty) {
+        //arrange
+        int actualQty;
+
+        //act
+        actualQty = phoneBook.add(name, number);
+
+        //assert
+        Assertions.assertEquals(expectedQty, actualQty);
+    }
+
+    public static Stream<Arguments> checkAddMethodSource() {
+        return Stream.of(
+                Arguments.of("Thomas", "+7(926)322-15-62", 1),
+                Arguments.of("William", "+7(936)283-82-82", 2),
+                Arguments.of("Sophie", "+7(926)111-11-89", 3),
+                Arguments.of("Thomas", "+7(936)283-82-82", 3),
+                Arguments.of("William", "+7(926)111-11-89", 3)
+        );
+    }
 }
