b2a3f5e9e402fb438786ca4eca2827e534e57b1f
Andrew Boiko <boiko_andrew@mail.ru>
2023-10-08 19:06:28 +0300

    findByNumber implemented

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -5,23 +5,40 @@ import java.util.Map;
 
 public class PhoneBook {
     private Map<String, String> nameMap;
+    private Map<String, String> numberMap;
 
     public PhoneBook() {
         this.nameMap = new HashMap<>();
+        this.numberMap = new HashMap<>();
     }
 
+    public int getContactsQty() {
+        return numberMap.size();
+    }
     public int add(String name, String number) {
         if (nameMap.containsKey(name)) {
             System.out.println("Cannot add contact with duplicate name.");
         } else {
-            nameMap.put(name, number);
-            System.out.println("Contact added successfully.");
+            if (numberMap.containsKey(number)) {
+                System.out.println("Cannot add contact with duplicate number.");
+            } else{
+                nameMap.put(name, number);
+                numberMap.put(number, name);
+                System.out.println("Contact added successfully.");
+            }
         }
         System.out.println("Total number of contacts is " + nameMap.size() + ".\n");
         return nameMap.size();
     }
 
     public String findByNumber(String number) {
-        return null;
+        if (!numberMap.containsKey(number)) {
+            System.out.println("Number " + number + " does not exist in the phone book.\n");
+            return "Unknown number";
+        }
+        else {
+            System.out.println("Number " + number + " belongs to " + numberMap.get(number) + "\n");
+            return numberMap.get(number);
+        }
     }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,6 +1,5 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -8,35 +7,41 @@ import ru.netology.PhoneBook;
 
 import java.util.stream.Stream;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class PhoneBookTest {
-    PhoneBook phoneBook;
+    private static PhoneBook phoneBook;
 
     @BeforeAll()
-    public void beforeAll() {
+    public static void beforeAll() {
         phoneBook = new PhoneBook();
+        phoneBook.add("James", "+7(926)311-11-14");
+        phoneBook.add("David", "+7(999)555-55-46");
+        phoneBook.add("Danny", "+7(999)555-55-47");
     }
 
     @ParameterizedTest
     @MethodSource("parametrizedCheckAddMethodSource")
-    public void parametrizedCheckAdd(String name, String number, int expectedQty) {
+    public void parametrizedCheckAdd(String name, String number, int expectedQtyIncrement) {
         //arrange
-        int actualQty;
+        int actualQtyIncrement;
+        int qtyBefore;
+        int qtyAfter;
 
         //act
-        actualQty = phoneBook.add(name, number);
+        qtyBefore = phoneBook.getContactsQty();
+        qtyAfter = phoneBook.add(name, number);
+        actualQtyIncrement = qtyAfter - qtyBefore;
 
         //assert
-        Assertions.assertEquals(expectedQty, actualQty);
+        Assertions.assertEquals(expectedQtyIncrement, actualQtyIncrement);
     }
 
     public static Stream<Arguments> parametrizedCheckAddMethodSource() {
         return Stream.of(
                 Arguments.of("Thomas", "+7(926)322-15-62", 1),
-                Arguments.of("William", "+7(936)283-82-82", 2),
-                Arguments.of("Sophie", "+7(926)111-11-89", 3),
-                Arguments.of("Thomas", "+7(936)283-82-82", 3),
-                Arguments.of("William", "+7(926)111-11-89", 3)
+                Arguments.of("William", "+7(936)283-82-82", 1),
+                Arguments.of("Sophie", "+7(926)111-11-89", 1),
+                Arguments.of("Thomas", "+7(936)283-82-82", 0),
+                Arguments.of("William", "+7(926)111-11-89", 0)
         );
     }
 
@@ -55,9 +60,9 @@ public class PhoneBookTest {
 
     public static Stream<Arguments> parametrizedCheckFindByNumberMethodSource() {
         return Stream.of(
-                Arguments.of("+7(926)322-15-62", "Thomas"),
-                Arguments.of("+7(936)283-82-82", "William"),
-                Arguments.of("+7(926)111-11-89", "Sophie"),
+                Arguments.of("+7(926)311-11-14", "James"),
+                Arguments.of("+7(999)555-55-46", "David"),
+                Arguments.of("+7(999)555-55-47", "Danny"),
                 Arguments.of("+7(936)258-82-82", "Unknown number"),
                 Arguments.of("+7(936)333-33-60", "Unknown number")
         );
